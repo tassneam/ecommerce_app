0410942c229f42dd6fe845cc7355f86659a2993a
Tassneam <tassneammohsen@hotmail.com>
2024-09-16 00:30:01 +0300

    add Homescreen Fragment

---
+++ app/src/main/java/com/example/ecommerce_app/fragments/HomescreenFragment.kt
@@ -0,0 +1,21 @@
+package com.example.ecommerce_app.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ecommerce_app.R
+
+
+class HomescreenFragment : Fragment() {
+
+override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_homescreen, container, false)
+    }
+
+}
