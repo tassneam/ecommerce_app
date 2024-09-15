1a80d1bb29645c1819758ef3523d628196b4cfa3
Tassneam <tassneammohsen@hotmail.com>
2024-09-15 15:35:01 +0300

    add base

---
+++ app/src/main/java/com/example/ecommerce_app/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecommerce_app.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ecommerce_app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
