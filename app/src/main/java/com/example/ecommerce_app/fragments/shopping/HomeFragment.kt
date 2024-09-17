2cdf10a346848e4f67107f83baf4f2a8ed898c19
Tassneam <tassneammohsen@hotmail.com>
2024-09-17 21:39:16 +0300

    add new home fragment

--- app/src/main/java/com/example/ecommerce_app/fragments/shopping/HomeFragment.kt
+++ app/src/main/java/com/example/ecommerce_app/fragments/shopping/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.example.ecommerce_app.fragments
+package com.example.ecommerce_app.fragments.shopping
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,14 +8,14 @@ import android.view.ViewGroup
 import com.example.ecommerce_app.R
 
 
-class HomescreenFragment : Fragment() {
+class HomeFragment : Fragment() {
 
 override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_homescreen, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
 }
