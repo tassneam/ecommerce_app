9b8df804ba463be21893f7bf13bc8acfc2604c65
Tassneam <tassneammohsen@hotmail.com>
2024-09-21 03:09:07 +0300

    update details Activity and navgraph

--- app/src/main/java/com/example/ecommerce_app/activities/DetailsActivity.kt
+++ app/src/main/java/com/example/ecommerce_app/activities/DetailsActivity.kt
@@ -1,18 +1,47 @@
 package com.example.ecommerce_app.activities
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ecommerce_app.fragments.shopping.DescriptionFragment
 import com.example.ecommerce_app.R
+import com.example.ecommerce_app.fragments.shopping.ReviewsFragment
+import com.example.ecommerce_app.adapters.DetailsPagerAdapter
+import com.example.ecommerce_app.adapters.SizeAdapter
 import com.example.ecommerce_app.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
-    lateinit var binding:ActivityDetailsBinding
+    lateinit var binding: ActivityDetailsBinding
+    lateinit var sizeAdapter: SizeAdapter
+    lateinit var sizeList: List<String>
+
+    lateinit var detailsPagerAdapter:DetailsPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityDetailsBinding.inflate(layoutInflater)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        sizeList = listOf("S", "M", "L", "XL")
+        sizeAdapter = SizeAdapter(sizeList)
+        val recyclerViewSize = findViewById<RecyclerView>(R.id.recyclerViewSize)
+        recyclerViewSize.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerViewSize.adapter = sizeAdapter
+
+
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
+
+        val fragments = listOf(
+            DescriptionFragment(),
+            ReviewsFragment()
+        )
+
+        detailsPagerAdapter = DetailsPagerAdapter(supportFragmentManager, fragments)
+        binding.viewPager.adapter = detailsPagerAdapter
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
+
     }
 }
