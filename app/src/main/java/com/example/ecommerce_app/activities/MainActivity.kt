33c3c82760934e0cdfe8bb3f4d10d711c8b883a3
Tassneam <tassneammohsen@hotmail.com>
2024-09-22 01:03:49 +0300

    add nav between fragments

--- app/src/main/java/com/example/ecommerce_app/activities/MainActivity.kt
+++ app/src/main/java/com/example/ecommerce_app/activities/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.ecommerce_app.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.ecommerce_app.R
 import com.example.ecommerce_app.databinding.ActivityMainBinding
 
@@ -9,7 +11,13 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // Find the NavController from the NavHostFragment
+        val navController = findNavController(R.id.shoppingHostFragment)
+
+        // Setup BottomNavigationView with NavController
+        binding.bottomNavigation.setupWithNavController(navController)
     }
+
 }
