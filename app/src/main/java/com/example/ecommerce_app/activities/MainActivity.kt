ec2c0356824d4cfdee186a909c1cada81f7e8ce4
Tassneam <tassneammohsen@hotmail.com>
2024-09-22 22:26:11 +0300

    modify bottom nav

--- app/src/main/java/com/example/ecommerce_app/activities/MainActivity.kt
+++ app/src/main/java/com/example/ecommerce_app/activities/MainActivity.kt
@@ -9,15 +9,46 @@ import com.example.ecommerce_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         // Find the NavController from the NavHostFragment
         val navController = findNavController(R.id.shoppingHostFragment)
-
-        // Setup BottomNavigationView with NavController
         binding.bottomNavigation.setupWithNavController(navController)
+        binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_homeFragment -> {
+                    if (navController.currentDestination?.id != R.id.homeFragment) {
+                        navController.navigate(R.id.homeFragment)
+                    }
+                    true
+                }
+                R.id.action_searchFragment -> {
+                    if (navController.currentDestination?.id != R.id.searchFragment) {
+                        navController.navigate(R.id.searchFragment)
+                    }
+                    true
+                }
+                R.id.action_cartFragment -> {
+                    // Handle navigation for cartFragment
+                    // navController.navigate(R.id.cartFragment)
+                    true
+                }
+                R.id.action_favouriteFragment -> {
+                    // Handle navigation for favouriteFragment
+                    // navController.navigate(R.id.favouriteFragment)
+                    true
+                }
+                R.id.action_profileFragment -> {
+                    // Handle navigation for profileFragment
+                    // navController.navigate(R.id.profileFragment)
+                    true
+                }
+                else -> false
+            }
+        }
     }
-
 }
