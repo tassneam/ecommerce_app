3a302b0048543a4f0656fde3e4e96316118b8b3b
Tassneam <tassneammohsen@hotmail.com>
2024-09-30 16:22:09 +0300

    add clicklistener to item adapter

--- app/src/main/java/com/example/ecommerce_app/adapters/ItemAdapter.kt
+++ app/src/main/java/com/example/ecommerce_app/adapters/ItemAdapter.kt
@@ -1,13 +1,18 @@
 package com.example.ecommerce_app.adapters
 
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.ecommerce_app.activities.DetailsActivity
 import com.example.ecommerce_app.databinding.CardLayoutBinding
 import com.example.ecommerce_app.models.Item
 
-class ItemAdapter(var c:Context, var itemList: ArrayList<Item>) :
+class ItemAdapter(var c: Context, var itemList: ArrayList<Item>) :
     RecyclerView.Adapter<ItemAdapter.ItemHolder>() {
     inner class ItemHolder(val binding: CardLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bindData(item: Item) {
@@ -21,7 +26,22 @@ class ItemAdapter(var c:Context, var itemList: ArrayList<Item>) :
     }
 
     override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+        val newList = itemList[position]
         holder.bindData(itemList[position])
+        //set data for details activity
+        Log.d("ItemAdapter", "Clicked Item - Title: ${newList.title}, Price: ${newList.price}, Rating: ${newList.rating}, Rating Count: ${newList.ratingCount}")
+
+
+        holder.binding.root.setOnClickListener {
+            val intent = Intent(c, DetailsActivity::class.java).apply {
+                putExtra("imageUrl", newList.imageUrl)
+                putExtra("price", newList.price.toString())
+                putExtra("title", newList.title)
+                putExtra("rating", newList.rating.toString())
+                putExtra("ratingCount", newList.ratingCount.toString())
+            }
+            c.startActivity(intent)
+        }
     }
 
     override fun getItemCount() = itemList.size
