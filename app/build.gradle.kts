7ae3e648d94317030b9844882aebed9f139d54e5
mrym9 <mariem40383@fci.bu.edu.eg>
2024-10-08 18:23:44 +0300

    setting fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,32 +1,24 @@
 plugins {
-
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id ("kotlin-kapt")
-    id ("kotlin-parcelize")
-    id("com.google.devtools.ksp")
-    id ("androidx.navigation.safeargs")
-    alias(libs.plugins.google.gms.google.services)
+   // id("org.jetbrains.kotlin.android")
+    // id("org.jetbrains.kotlin.android")
+    //id("org.jetbrains.kotlin.android")
 }
-
 android {
     buildFeatures{
         viewBinding=true
-        dataBinding=true
     }
     namespace = "com.example.ecommerce_app"
     compileSdk = 34
-
     defaultConfig {
         applicationId = "com.example.ecommerce_app"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -44,52 +36,19 @@ android {
         jvmTarget = "1.8"
     }
 }
-
 dependencies {
-
-    implementation(libs.firebase.database)
-    val nav_version = "2.7.5"
-    implementation ("androidx.navigation:navigation-fragment-ktx:$nav_version")
-    implementation ("androidx.navigation:navigation-ui-ktx:$nav_version")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation("com.google.android.material:material:1.10.0")
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.activity:activity:1.9.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    // ROOM
-    val roomVersion = "2.6.1"
-    implementation ("androidx.room:room-runtime:$roomVersion")
-    ksp("androidx.room:room-compiler:$roomVersion")
-    // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
-    implementation ("androidx.room:room-ktx:$roomVersion")
-    // Life Cycle Arch
-    val lifecycleVersion = "2.6.2"
-    // ViewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
-    // LiveData
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
-    // Annotation processor
-    ksp("androidx.lifecycle:lifecycle-compiler:$lifecycleVersion")
+    implementation("com.zeugmasolutions.localehelper:locale-helper-android:1.5.1")
 
-    implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.0.0")
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
-
-    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
-    implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-firestore")
-    val lifecycle_version = "2.2.0"
-    //    call API
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-//dependency injection
-    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
-}
-kapt {
-    correctErrorTypes = true
 }
