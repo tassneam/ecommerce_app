20f1307e3ba36b0ef14457607d261e8bf32c22ba
Tassneam <tassneammohsen@hotmail.com>
2024-10-09 20:18:10 +0300

    fix nav

--- app/src/main/java/com/example/ecommerce_app/activities/MainActivity.kt
+++ app/src/main/java/com/example/ecommerce_app/activities/MainActivity.kt
@@ -15,41 +15,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         // Find the NavController from the NavHostFragment
         val navController = findNavController(R.id.shoppingHostFragment)
+
+        // Set up the bottom navigation with the NavController
         binding.bottomNavigation.setupWithNavController(navController)
-        binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.action_homeFragment -> {
-                    if (navController.currentDestination?.id != R.id.homeFragment) {
-                        navController.navigate(R.id.homeFragment)
-                    }
-                    true
-                }
-                R.id.action_searchFragment -> {
-                    if (navController.currentDestination?.id != R.id.searchFragment) {
-                        navController.navigate(R.id.searchFragment)
-                    }
-                    true
-                }
-                R.id.action_cartFragment -> {
-                    // Start CartActivity when the cart item is selected
-                    val intent = Intent(this, Cart::class.java)
-                    startActivity(intent)
-                    true
-                }
-                R.id.action_favouriteFragment -> {
-                    // Handle navigation for favouriteFragment
-                    // navController.navigate(R.id.favouriteFragment)
-                    true
-                }
-                R.id.action_profileFragment -> {
-                    if (navController.currentDestination?.id != R.id.settingFragment) {
-                        navController.navigate(R.id.settingFragment)
-                    }
-                    true
-                }
-                else -> false
+
+        // Handle CartActivity separately since it's not part of the nav graph
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            if (item.itemId == R.id.action_cartFragment) {
+                val intent = Intent(this, Cart::class.java)
+                startActivity(intent)
+                true
+            } else {
+                // Allow the navigation to continue for other fragments
+                navController.navigate(item.itemId)
+                true
             }
         }
     }
