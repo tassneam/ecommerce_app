98958170e83d74a3e5f80169b20e4a1b10ad4b28
Tassneam <tassneammohsen@hotmail.com>
2024-10-09 19:16:52 +0300

    fix conflicts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id ("com.android.library") version "7.3.1" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+    alias(libs.plugins.google.gms.google.services) apply false
+}
+buildscript {
+    repositories {
+        google()
+    }
 
+    dependencies {
+        val navVersion = "2.7.5"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+    }
 }
