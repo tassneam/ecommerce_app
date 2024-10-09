4b0bac34a2c2436accd8b85b41fdd94d620b7a62
Tassneam <tassneammohsen@hotmail.com>
2024-10-09 20:38:32 +0300

    add logout

--- app/src/main/java/com/example/ecommerce_app/fragments/shopping/SettingFragment.kt
+++ app/src/main/java/com/example/ecommerce_app/fragments/shopping/SettingFragment.kt
@@ -1,14 +1,21 @@
 package com.example.ecommerce_app.fragments.shopping
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.ecommerce_app.activities.SignInActivity
 import com.example.ecommerce_app.databinding.FragmentSettingBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 
 class SettingFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth;
+
     lateinit var binding: FragmentSettingBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +23,17 @@ class SettingFragment : Fragment() {
     ): View? {
         binding = FragmentSettingBinding.inflate(layoutInflater)
         return binding.root
+        auth = Firebase.auth
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.logoutLayout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(requireContext(), SignInActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            requireActivity().finish()
+        }
+    }
 }
